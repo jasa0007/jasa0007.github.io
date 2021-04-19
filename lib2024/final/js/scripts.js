1fe4a4bddab4dd907f269728fb6f4047f97b3a95
jasa0007 <77303015+jasa0007@users.noreply.github.com>
2021-04-19 18:03:24 -0400

    final

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,10 +1,13 @@
-var form = document.getElementById("infoBox"); 
-
+//submit button action
+var submitBox = document.getElementById("infoBox"); 
+// prevent invaild inputs
 function formSubmiter(event) {event.preventDefault();} 
+// add event listener to submit button
+submitBox.addEventListener('submit', formSubmiter);
 
-form.addEventListener('submit', formSubmiter);
-
+// handle all var input boxes along with user added inputs
 function variableHandalerJS(){
+	// variable declerations of user elements
 	var name = document.getElementById('name');
 	var email = document.getElementById('email');
 	var address = document.getElementById('address');
@@ -17,7 +20,7 @@ function variableHandalerJS(){
 	var changedCity = document.getElementById('userCity');
 	var changedProvince = document.getElementById('userProvince');
 	var changedPostCode = document.getElementById('userPostCode');
-
+// assign user input values into new values to display
 name.innerHTML = changedName.value;
 email.innerHTML = changedEmail.value;
 address.innerHTML = changedAddress.value;
@@ -26,4 +29,6 @@ province.innerHTML = changedProvince.value;
 postCode.innerHTML = changedPostCode.value;
 	}
 
+// the javascript validator is checking only the relations in this js file
+// it is not counting the one in the html where the variableHandalerJs is connected too
 		
